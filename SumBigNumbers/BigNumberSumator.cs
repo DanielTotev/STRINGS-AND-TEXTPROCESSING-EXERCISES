1e5f7ad8eb035e32bb754793a4aff45d57c45650
DanielTotev <dtotev1@gmail.com>
2017-06-28 18:21:48 +0300

    Added some more solved problems

---
+++ SumBigNumbers/BigNumberSumator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace SumBigNumbers
+{
+    public class BigNumberSumator
+    {
+        public static void Main()
+        {
+            var firstNumberAsString = Console.ReadLine().TrimStart(new char[] { '0' });
+            var secondNumberAsString = Console.ReadLine().TrimStart(new char[] { '0' });
+            var result = SumNumbers(firstNumberAsString, secondNumberAsString);
+            Console.WriteLine(result);
+        }
+
+        public static StringBuilder SumNumbers(string firstNumberAsString, string secondNumberAsString)
+        {
+            var biggerNumber = new StringBuilder();
+            var smallerNumber = new StringBuilder();
+
+            if (secondNumberAsString.Length >= firstNumberAsString.Length)
+            {
+                biggerNumber.Append(secondNumberAsString);
+                smallerNumber.Append(new string('0', secondNumberAsString.Length - firstNumberAsString.Length)).Append(firstNumberAsString);
+            }
+            else
+            {
+                biggerNumber.Append(firstNumberAsString);
+                smallerNumber.Append(new string('0', firstNumberAsString.Length - secondNumberAsString.Length)).Append(secondNumberAsString);
+            }
+
+            var residue = 0;
+            var result = new StringBuilder();
+
+            for (int i = biggerNumber.Length - 1; i >= 0; i--)
+            {
+                var biggerNumDigit = int.Parse(biggerNumber[i].ToString());
+                var smallerNumDigit = int.Parse(smallerNumber[i].ToString());
+                var sum = biggerNumDigit + smallerNumDigit + residue;
+
+                if (sum <= 9)
+                {
+                    result.Append(sum);
+                    residue = 0;
+                }
+                else
+                {
+                    result.Append(sum % 10);
+                    residue = sum / 10;
+                }
+            }
+
+            if (residue != 0)
+            {
+                result.Append(residue);
+            }
+
+            result = Reverse(result);
+
+            return result;
+        }
+        public static StringBuilder Reverse(StringBuilder text)
+        {
+       
+            StringBuilder reverse = new StringBuilder();
+            for (int i = text.Length - 1; i > -1; i--)
+            {
+                reverse.Append(text[i]);
+            }
+            return reverse;
+        }
+    }
+}
---
+++ UnicodeCharacters/Unicode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+namespace UnicodeCharacters
+{
+    public class Unicode
+    {
+        public static void Main()
+        {
+            var text = Console.ReadLine();
+            var result = GetUniCode(text);
+            Console.WriteLine(result);
+        }
+
+        public static StringBuilder GetUniCode(string text)
+        {
+            var result = new StringBuilder();
+            foreach (var character in text)
+            {
+                var unicode = GetEscapeSequence(character);
+                result.Append(unicode);
+            }
+
+            return result;
+        }
+        public static string GetEscapeSequence(char c)
+        {
+            return "\\u" + ((int)c).ToString("x4");
+        }
+    }
+}
