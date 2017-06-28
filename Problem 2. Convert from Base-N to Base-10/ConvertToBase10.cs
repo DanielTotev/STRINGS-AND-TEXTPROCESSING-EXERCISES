ddd4c5957b4e1386734ee91762be3d0de4f60dd2
DanielTotev <dtotev1@gmail.com>
2017-06-28 18:20:52 +0300

    Added some more solved problems

---
+++ Problem 2. Convert from Base-N to Base-10/ConvertToBase10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace Problem_2.Convert_from_Base_N_to_Base_10
+{
+    public class ConvertToBase10
+    {
+        public static void Main()
+        {
+            var inputParams = Console.ReadLine().Split();
+            var numberAsString = inputParams[1];
+            var currentBase = int.Parse(inputParams[0]);
+            var numberInBase10 = Calculate(numberAsString, currentBase);
+            Console.WriteLine(numberInBase10);
+            
+        }
+
+        public static BigInteger Calculate(string numberAsString, int currentBase)
+        {
+            var NumberAsSrtingReversed = Reverse(numberAsString);
+            BigInteger result = 0;
+            for (int i = 0; i < NumberAsSrtingReversed.Length; i++)
+            {
+                var digit = int.Parse(NumberAsSrtingReversed[i].ToString());
+                result  += digit * BigInteger.Pow(currentBase, i);
+            }
+            
+            return result;
+        }
+
+        public static string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+    }
+}
