929168e767f04e1509e42739c24f9201ae44aa64
DanielTotev <dtotev1@gmail.com>
2017-06-28 18:15:52 +0300

    Added some more sloved problems

---
+++ CharacterMultiplier/MultiplyCharacters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+namespace CharacterMultiplier
+{
+    public class MultiplyCharacters
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+            var result = CalculateStrings(input[0], input[1]);
+            Console.WriteLine(result);
+        }
+
+        public static BigInteger CalculateStrings(string firstString, string secondString)
+        {
+            BigInteger result = 0;
+
+            var minLength = Math.Min(firstString.Length, secondString.Length);
+            for (int i = 0; i < minLength; i++)
+            {
+                int firstValue = firstString[i];
+                int secondValue = secondString[i];
+                result += (firstValue * secondValue);
+            }
+            if (firstString.Length > secondString.Length)
+            {
+                var initalString = firstString.Substring(minLength);
+                for (int i = 0; i < initalString.Length; i++)
+                {
+                    result += initalString[i];
+                }
+            }
+            else if (secondString.Length > firstString.Length)
+            {
+                var initalString = secondString.Substring(minLength);
+                for (int i = 0; i < initalString.Length; i++)
+                {
+                    result += initalString[i];
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Convert from Base-10 to Base-N/Convertor.cs
@@ -0,0 +1,44 @@
+﻿namespace Convert_from_Base_10_to_Base_N
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+
+    public class Convertor
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+            var baseToConvert = input[0];
+            var numberToConvert = input[1];
+            var result = CalculateNumber(numberToConvert, baseToConvert);
+            Console.WriteLine(result);
+        }
+
+        public static string CalculateNumber(BigInteger number, BigInteger baseToConvert)
+        {
+            var reversedResult = new StringBuilder();
+            while (number > 0)
+            {
+                var digitToAppend = number % baseToConvert;
+                reversedResult.Append(digitToAppend);
+                number /= baseToConvert;
+            }
+            var finalResult = Reverse(reversedResult.ToString());
+
+            return finalResult;
+        }
+
+        public static string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+    }
+}
