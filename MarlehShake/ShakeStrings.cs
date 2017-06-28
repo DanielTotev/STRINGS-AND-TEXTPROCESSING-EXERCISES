ba1ea6f03903fdb4d4c857c748b5de71c18cf059
DanielTotev <dtotev1@gmail.com>
2017-06-28 18:19:48 +0300

    Added some more sloved problems

---
+++ MarlehShake/ShakeStrings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MarlehShake
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var key = Console.ReadLine();
+
+            while (true)
+            {
+                var leftIndex = input.IndexOf(key);
+                var rightIndex = input.LastIndexOf(key);
+
+                var leftIndexExists = leftIndex != -1;
+                var rightIndexExists = rightIndex != -1;
+                var keyIsNotEmpty = key != string.Empty;
+
+                var indexesAreEqual = leftIndex == rightIndex;
+
+                if (leftIndexExists && rightIndexExists && !indexesAreEqual && keyIsNotEmpty)
+                {
+                    input = input.Remove(rightIndex, key.Length);
+                    input = input.Remove(leftIndex, key.Length);
+                  
+                    Console.WriteLine("Shaked it.");
+                    key = key.Remove(key.Length / 2, 1);
+                }
+                else
+                {
+                    Console.WriteLine("No shake.");
+                    Console.WriteLine(input);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ MultiplyBigNumber/BigNumberMultiplier.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+namespace MultiplyBigNumber
+{
+    public class BigNumberMultiplier
+    {
+        public static void Main()
+        {
+            var number = Console.ReadLine().TrimStart(new char[] { '0' });
+            var digitAsString = Console.ReadLine().TrimStart(new char[] { '0' });
+            var digit = 0;
+            int.TryParse(digitAsString, out digit);
+            var result = Multiply(number, digit);
+            Console.WriteLine(result);
+        }
+
+        public static StringBuilder Multiply(string number, int digit)
+        {
+            var reversedResult = new StringBuilder();
+            var currentProduct = 0;
+            var addition = 0;
+            if (digit == 0)
+            {
+                reversedResult.Append(0);
+                return reversedResult ;
+            }
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                var currentNumDigit = int.Parse(number[i].ToString());
+                currentProduct = (currentNumDigit * digit) + addition;
+                if (currentProduct <= 9)
+                {
+                    reversedResult.Append(currentProduct);
+                    addition = 0;
+                }
+                else
+                {
+                    reversedResult.Append(currentProduct % 10);
+                    addition = currentProduct / 10;
+                }
+            }
+            if (addition != 0)
+            {
+                reversedResult.Append(addition);
+            }
+            var result = Reverse(reversedResult);
+            return result;
+        }
+        public static StringBuilder Reverse(StringBuilder text)
+        {
+
+            StringBuilder reverse = new StringBuilder();
+            for (int i = text.Length - 1; i > -1; i--)
+            {
+                reverse.Append(text[i]);
+            }
+            return reverse;
+        }
+    }
+}
