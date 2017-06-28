060442c5f65308533d0982f86096e066d86f6aa6
DanielTotev <dtotev1@gmail.com>
2017-06-28 18:18:38 +0300

    Added some more sloved problems

---
+++ LettersChangeNumbers/LettersToNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LettersChangeNumbers
+{
+    public class LettersToNumbers
+    {
+        public static void Main()
+        {
+            var specialWords = Console.ReadLine().Split(new[] {' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var sum = 0m;
+            for (int i = 0; i < specialWords.Length; i++)
+            {
+                var currentWord = specialWords[i];
+                sum += CalculateWord(currentWord);
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+
+        public static decimal CalculateWord(string word)
+        {
+            var firstLetter = word[0];
+            var secondLetter = word[word.Length - 1];
+            var number = decimal.Parse(word.Substring(1, word.Length - 2));
+            if (char.IsUpper(firstLetter))
+            {
+                var charPosition = (firstLetter - ('A' - 1));
+                number /= charPosition;
+            }
+            else
+            {
+                number *= (firstLetter - ('a' - 1));
+            }
+
+            if (char.IsUpper(secondLetter))
+            {
+                number -= (secondLetter - ('A' - 1));
+            }
+            else
+            {
+                number += (secondLetter - ('a' - 1));
+            }
+            return number;
+        }
+    }
+}
---
+++ MagicExchangeableWords/CheckExchangeableWords.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MagicExchangeableWords
+{
+    public class CheckExchangeableWords
+    {
+        public static void Main()
+        {
+            var words = Console.ReadLine().Split();
+            var firstWord = words[0];
+            var secondWord = words[1];
+            int len = Math.Min(firstWord.Length, secondWord.Length);
+            var map = new Dictionary<char, char>();
+            for (int i = 0; i < len; i++)
+            {
+                if (!map.ContainsKey(firstWord[i]))
+                {
+                    if (map.ContainsValue(secondWord[i]))
+                    {
+                        Console.WriteLine("false");
+                        return;
+                    }
+                    map[firstWord[i]] = secondWord[i];
+                }
+                else
+                {
+                    if (map[firstWord[i]] != secondWord[i])
+                    {
+                        Console.WriteLine("false");
+                        return;
+                    }
+                }
+
+            }
+
+            if (firstWord.Length == secondWord.Length)
+            {
+                Console.WriteLine("true");
+            }
+
+            if (firstWord.Length != secondWord.Length)
+            {
+                var longerWord = firstWord.Length > secondWord.Length ? firstWord : secondWord;
+                var subStr = longerWord.Substring(len);
+                var isContained = true;
+                foreach (var ch in subStr)
+                {
+                    if (!(map.ContainsKey(ch) || map.ContainsValue(ch)))
+                    {
+                        isContained = false;
+                        break;
+                    }
+                }
+
+                if (isContained)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+          
+        }
+    }
+}
